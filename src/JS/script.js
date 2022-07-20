30badb44043e303f9bc05611ed9cb9cb0506ee62
Alfredo <alfredofontinele08@gmail.com>
2022-07-19 21:13:46 -0300

    update

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body")
 const inputText = document.getElementById("pesquisa")
 const button = document.getElementById("adicionar-tarefa")
 const qtd_tarefas = document.getElementById("quantidade-tarefas")
@@ -58,7 +59,6 @@ function removerTarefa(event) {
 let id = 0
 
 function editarTarefa(event) {
-    const body = document.querySelector("body")
     const section_alteracao = document.getElementById("section-alteracao")
     const alterar = document.getElementById("img-alterar-tarefa")
     const manter = document.getElementById("img-manter-tarefa")
@@ -72,7 +72,7 @@ function editarTarefa(event) {
                 const section = document.querySelector(".section-alert")
                 section.style.display = "flex"
                 section.innerHTML = `
-                    <div class="camp-alert" id="camp-alert">
+                    <div class="camp-alert">
                         <h2>Campo não pode estar vazio</h2>
                         <img class="closed" src="https://cdn-icons-png.flaticon.com/512/18/18297.png" alt="botao | fechar">
                     <div>
@@ -82,7 +82,6 @@ function editarTarefa(event) {
                     let localEvento = event.target
                     if (localEvento.tagName == "IMG") {
                         section.style.display = "none"
-                        section.innerHTML = ""
                     }
                 })
             } else {
@@ -101,8 +100,33 @@ function editarTarefa(event) {
 function limparTarefas(event) {
     const localEvento = event.target
     if (localEvento.classList.contains("limpar-tarefas")) {
-        arrayTarefas = []
-        listarTarefas(arrayTarefas, lista)
+        const section = document.createElement("section")
+        section.style.display = "flex"
+        section.classList.add("section-limpeza")
+        section.innerHTML = `
+            <div class="camp-limpeza">
+                <h2>Você tem certeza?</h2>
+                <div class="confirmar-limpeza">
+                    <img class="confirmar" src="https://cdn-icons-png.flaticon.com/512/25/25179.png" alt="botao | confirmação">
+                    <img class="fechar" src="https://cdn-icons-png.flaticon.com/512/18/18297.png" alt="botao | fechar">
+                <div>
+            <div>
+        `
+        body.append(section)
+
+        const confirmar = document.querySelector(".confirmar")
+        const fechar = document.querySelector(".confirmar")
+
+        section.addEventListener("click", (event) => {
+            const localEvento = event.target
+            if (localEvento.classList.contains("confirmar")) {
+                arrayTarefas = []
+                listarTarefas(arrayTarefas, lista)
+                section.style.display = "none";
+            } else if (localEvento.classList.contains("fechar")) {
+                section.style.display = "none";
+            }
+        })
     }
 }
 
